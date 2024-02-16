a6755242884efb6ce08e88efb4297a8d0549150b
GitPoomCmu <152594220+GitPoomCmu@users.noreply.github.com>
2024-02-16 17:19:57 +0700

    Update lab20_1.cpp

--- lab20_1.cpp
+++ lab20_1.cpp
@@ -20,20 +20,68 @@ string toUpperStr(string x){
     return y;
 }
 
-void importDataFromFile(){
-
+void importDataFromFile(string file, vector<string> &names, vector<int> &scores, vector<char> &grades){
+    ifstream source;
+    source.open(file);
+    
+    string textline;
+    
+    char format[] = {"%[^:]: %d %d %d"};
+    char name[99];
+    int grade1, grade2, grade3, total;
+    char grade;
+    
+    while(getline(source, textline)){
+        sscanf(textline.c_str(), format, name, &grade1, &grade2, &grade3);
+        total = grade1 + grade2 + grade3;
+        grade = score2grade(total);
+        
+        names.push_back(name);
+        scores.push_back(total);
+        grades.push_back(grade);
+    }
 }
 
-void getCommand(){
-
+void getCommand(string &command, string &key){
+    string input;
+    cout << "Please input your command: ";
+    cin >> command;
+    input = toUpperStr(command);
+    if(input == "GRADE" || input == "NAME"){
+        cin.ignore();
+        getline(cin, key);
+    }
 }
 
-void searchName(){
-
+void searchName(vector<string> names, vector<int> scores, vector<char> grades, string key){
+    bool found;
+    cout << "---------------------------------\n";
+    for(int unsigned i=0; i < names.size(); i++){
+        if(toUpperStr(names[i]) == key){
+            found = true;
+            printf("%s's score = %d\n%s's grade = ", names[i].c_str(), scores[i], names[i].c_str());
+            cout << grades[i] << "\n";
+        }
+    }
+    if(found == false){
+        cout << "Cannot found.\n";
+    }
+    cout << "---------------------------------\n";
 }
 
-void searchGrade(){
-
+void searchGrade(vector<string> &names, vector<int> &scores, vector<char> &grades, string key){
+    bool found;
+    cout << "---------------------------------\n";
+    for(int unsigned i=0; i<grades.size(); i++){
+        if(grades[i] == key.c_str()[0]){
+            found = true;
+            printf("%s (%d)\n", names[i].c_str(), scores[i]);
+        }
+    }
+    if(found == false){
+        cout << "Cannot found.\n";
+    }
+    cout << "---------------------------------\n";
 }
 
 
